36a1f6986893e40cc9919e6ab861c518b7d6a4f8
Eurus Holmes <17801114068@163.com>
2018-10-09 09:56:43 +0800

    add 3rd class project

---
+++ My_Messager/app/src/main/java/bjfu/it/cfy/my_messager/CreateMessageActivity.java
@@ -0,0 +1,24 @@
+package bjfu.it.cfy.my_messager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    public void onSendMessage(View view){
+        // Todo the 2nd Activity
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
